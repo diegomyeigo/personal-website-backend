6ed0084435905b6399faae34fbaf0d9ede68d5ed
Jose Perez <jdiegoperez001@gmail.com>
2026-08-08 19:17:22 -0500

    Testing frontend connection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,17 @@ CORS(app)
 @app.route('/api/test')
 def test():
     return {"message": "Successful test"}
+
+@app.route("/api/survey", methods=["POST"])
+def submit_survey():
+    data = request.get_json()
+
+    print(data)
+
+    return {
+        "success": True,
+        "message": "Received!"
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
