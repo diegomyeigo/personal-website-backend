25018847b279e7af0c819abcb6f00d02bc38fcd3
Jose Perez <jdiegoperez001@gmail.com>
2026-08-15 18:30:18 -0500

    Multipart email test

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import psycopg
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_mail import Mail, Message
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
 
@@ -122,32 +124,33 @@ def submit_survey():
         }), 422
 
     user_email = database_record["email"]
-    message = Message(
-        subject="Thank you for taking my survey!",
-        # body="Thanks for taking the time to complete my survey!\nYou're alright ;)\n\n",
-        recipients=[user_email],
-        sender="jdiegoperez001@gmail.com"
-    )
+    root = MIMEMultipart('related')
+    root['Subject'] = "Thank you for taking my survey!"
+    root['From'] = "jdiegoperez001@gmail.com"
+    root['To'] = user_email
 
-    message.html = """
+    html = """
     <html>
         <body>
             <p><b>**This is an automated message. Please do not respond**</b></p>
             <h1>Thank you!</h1>
-            <br>
             <p>Thanks for taking the time to complete my survey</p>
             <p>You're alright ;)</p>
-            <br>
             <img src="cid:rigby" alt="Rigby the cat" width="300" height="300">
         </body>
     </html>
     """         
 
-    with app.open_resource("email_assets/rigby.jpg") as img:
+    html_part = MIMEText(html, 'html')
+    root.attach(html_part)
+
+    image_path = os.path.join(os.path.dirname(__file__), "email_assets", "rigby.jpg")
+
+    with open(image_path, "rb") as img:
         mime_img = MIMEImage(img.read(), _subtype="jpeg")
         mime_img.add_header("Content-ID", "<rigby>")
-        mime_img.add_header("Content-Disposition", "inline", filename="rigby.png")
-        message.attach(mime_img)
+        mime_img.add_header("Content-Disposition", "inline", filename="rigby.jpg")
+        root.attach(mime_img)
         # message.attach(
         #     filename="rigby.jpg",
         #     content_type="image/jpeg",
@@ -155,6 +158,13 @@ def submit_survey():
         #     disposition="inline",
         #     headers=[("Content-ID", "<rigby>")]
         # )
+
+        message = Message()
+        message.sender = "jdiegoperez001@gmail.com"
+        message.recipients = [user_email]
+        message.msg = root.as_string()
+
+        mail.send(message)
 
     try:
         mail.send(message)
